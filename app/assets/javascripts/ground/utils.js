1725f5064511607b929a9310dc924bae6b5d6b16
Adrien Folie <folie.adrien@gmail.com>
2014-10-04 15:58:29 +0200

    add php support

--- app/assets/javascripts/ground/utils.js
+++ app/assets/javascripts/ground/utils.js
@@ -24,6 +24,7 @@ var utils = {
         ['python3', 'python', 'print("Hello World")'],
         ['c', 'c_cpp', '#include <stdio.h>\r\n\r\nint main()\r\n{\r\n\tprintf("Hello World\\n");\r\n\treturn 0;\r\n}'],
         ['cpp', 'c_cpp', '#include <iostream>\r\n\r\nint main()\r\n{\r\n\tstd::cout << "Hello World\\n";\r\n\treturn 0;\r\n}'],
-        ['csharp', 'csharp', 'using System;\r\n\r\nclass Program\r\n{\r\n\tstatic void Main(string[] args)\r\n\t{\r\n\t\tConsole.WriteLine("Hello world");\r\n\t}\r\n}']
+        ['csharp', 'csharp', 'using System;\r\n\r\nclass Program\r\n{\r\n\tstatic void Main(string[] args)\r\n\t{\r\n\t\tConsole.WriteLine("Hello world");\r\n\t}\r\n}'],
+        ['php', 'php', '<?php\r\n\r\nprint("Hello world\\n");']
     ]
 };
