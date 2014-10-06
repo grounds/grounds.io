bdc4ef54728ac26ae02f41c7c67342553dccd7fd
Adrien Folie <folie.adrien@gmail.com>
2014-10-06 03:37:30 +0200

    updates gem dependencies

--- vendor/assets/javascripts/vendor_application.js
+++ vendor/assets/javascripts/vendor_application.js
@@ -4,14 +4,15 @@
 //= require foundation
 
 //= require ace/ace
+//= require ace/worker-html
+//= require ace/worker-php
+
 //= require ace/mode-ruby
 //= require ace/mode-golang
 //= require ace/mode-python
 //= require ace/mode-c_cpp
 //= require ace/mode-csharp
 //= require ace/mode-php
-
-//= require ace/worker-php
 
 //= require ace/theme-tomorrow_night
 //= require ace/theme-textmate
