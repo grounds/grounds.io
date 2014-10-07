e3f8532d711d12b3603ebeeeb8164da0657149c3
Adrien Folie <folie.adrien@gmail.com>
2014-10-07 03:56:00 +0200

    refactors some code smells (javascript)

--- app/assets/javascripts/ground/client.js
+++ app/assets/javascripts/ground/client.js
@@ -6,24 +6,24 @@ function Client(endpoint) {
 }
 
 Client.prototype.connect = function(endpoint) {
-    if (endpoint === null) return;
+    if (!endpoint) return;
 
     this._socket = io.connect(endpoint, {'forceNew':true });
     this.bindEvents();
 };
 
 Client.prototype.disconnect = function() {
-    if (this.connected() === false) return;
+    if (!this.connected()) return;
 
     this._socket.io.disconnect();
 };
 
 Client.prototype.connected = function() {
-    return this._socket !== null && this._socket.connected;
+    return this._socket && this._socket.connected;
 };
 
 Client.prototype.send = function(event, data) {
-    if (this.connected() === false) return;
+    if (!this.connected()) return;
 
     this._console.startWaiting();
 
@@ -34,11 +34,9 @@ Client.prototype.bindEvents = function() {
     var self = this;
     this._socket.on('run', function(data) {
         self._console.write(data.stream, data.chunk);
-    });
-    this._socket.on('connect', function(data) {
+    }).on('connect', function(data) {
         self._console.clean();
-    });
-    this._socket.on('connect_error', function() {
+    }).on('connect_error', function() {
         self._console.error();
     });
 };
