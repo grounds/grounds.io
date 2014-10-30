7ee630c03700123888a48f2c2e9eabce2bef0400
Adrien Folie <folie.adrien@gmail.com>
2014-10-30 23:57:33 +0000

    add link to status page

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,4 +1,5 @@
 MenuLinks = [
+  ['status page', I18n.t('site.status'), 'http://status.42grounds.io'],
   ['about page', I18n.t('site.about'), '/about'],
   ['project sources', I18n.t('site.sources'), 'https://github.com/grounds']
 ]
