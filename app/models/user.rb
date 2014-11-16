a6eeeee56ff5c9d1b2f9983c560b462533dabb74
Gaetan Gueraud <gaetan.gueraud@gmail.com>
2014-11-16 21:14:35 +0100

    Addition of Github login

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  authenticates_with_sorcery! do |config|
+    config.authentications_class = Authentication
+  end
+
+  has_many :authentications, :dependent => :destroy
+  accepts_nested_attributes_for :authentications
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   match 's/:id', to: 'grounds#shared', as: 'ground_shared', via: :get
   match 'grounds/share', to: 'grounds#share', as: 'ground_share', via: :post
   match 'grounds/switch_option', to: 'grounds#switch_option', as: 'ground_switch_option', via: :put
+  post "oauth/callback" => "oauths#callback"
+  get "oauth/callback" => "oauths#callback"
+  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
+  delete "oauth/:provider" => "oauths#destroy", :as => :delete_oauth
 end
