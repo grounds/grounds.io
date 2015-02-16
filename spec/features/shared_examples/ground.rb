0808bda131ce2a66d6d5d61ce27fd894fc710095
Adrien Folie <folie.adrien@gmail.com>
2015-02-16 11:37:59 +0100

    add shared examples for ground
    Connected example
    Disconnected  example

---
+++ spec/features/shared_examples/ground.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+shared_examples_for 'a ground without shared url' do
+  scenario 'ground has no visible link to a shared url', js: :true do
+    expect(ground.shared_url).to be_hidden
+  end
+end
+
+shared_examples_for 'a disconnected ground' do
+  scenario 'is not connected to runner' do
+    expect(ground).not_to be_connected
+  end
+
+  scenario 'console displays a connection error' do
+    expect(ground.console).to have_connection_error
+  end
+end
+
+shared_examples_for 'a connected ground' do
+  scenario 'ground is connected to runner' do
+    expect(ground).to be_connected
+  end
+end
+
--- spec/features/shared_examples/ground_without_shared_url.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-shared_examples_for 'a ground without shared url' do
-  scenario 'ground has no visible link to a shared url', js: :true do
-    expect(ground.shared_url).to be_hidden
-  end
-end
